79a836bac9847414e549b178281755327cc88f86
Alexandre Luu <alexandre.luu6@gmail.com>
2023-06-07 10:55:25 +0200

    fix index page

--- renderer/.next/static/chunks/webpack.js
+++ renderer/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a84eb76da6ffb035"; }
+/******/ 		__webpack_require__.h = function() { return "ce70582aa779a2a9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- renderer/pages/index.tsx
+++ renderer/pages/index.tsx
@@ -7,21 +7,21 @@ const IndexPage = () => {
     const handleMessage = (_event, args) => alert(args)
 
     // add a listener to 'message' channel
-    // global.ipcRenderer.addListener('message', handleMessage)
+    // ipc.addListener('message', handleMessage)
 
     return () => {
-      // global.ipcRenderer.removeListener('message', handleMessage)
+      // ipc.removeListener('message', handleMessage)
     }
   }, [])
 
   const onSayHiClick = () => {
-    // global.ipcRenderer.send('message', 'hi from next')
+    // ipc.send('message', 'hi from next')
   }
 
   return (
     <Layout title="Home | Next.js + TypeScript + Electron Example">
       <h1>Hello Next.js 👋</h1>
-      {/* <button onClick={onSayHiClick}>Say hi to electron</button> */}
+      <button onClick={onSayHiClick}>Say hi to electron</button>
       <p>
         <Link href="/about">About</Link>
       </p>
